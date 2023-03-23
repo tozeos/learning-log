59b1bc76db196c4130646fab514ca498f0a31693
tozu <tozeos@outlook.com>
2023-03-23 14:48:14 -0300

    updated src/Posts.js.

--- src/Posts.js
+++ src/Posts.js
@@ -1,7 +1,7 @@
 export const postagens = [
 	{
 		id: 0,
-		date: "March 21nd",
+		date: "March 22nd",
 		p1: "Today I learned soo many things, some stuff I already knew, but many of them I never had heard about. There's so many categories that I've known for so long and others that I haven't. Summarizing everything, I've learned how words are formed, lot's of them I knew, but It was funny knowing more about it.",
 		p2: "I really liked learning all of the ways a word can be formed, and how it changes (or not) the meaning. I kinda like the affixes, they are so usefull and, OMG I just thought about something: if you know the meaning of one of these, you instantly learn a bunch of new words, for example: the words ending -ful, is used for something in quantity, like useful, beautiful, painful. I really like that.",
 		p3: "I took some notes, so I think I got the main points of what the teacher showed us, but I have to study a little bit more by my self to fix all the concepts and examples in my mind. There was so many new concepts that I’m afraid of mix some of them and go crazy on the tests.",
