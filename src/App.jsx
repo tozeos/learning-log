b74985bc0777ad4fc6048d78041c7d83f6247436
Caike Laurentino <tozeos@outlook.com>
2023-03-23 12:58:07 -0300

    new log

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import logo from "./assets/logo.png";
 import leaf from "./assets/Leaf.png";
 import "./App.css";
+import Posts from "./posts";
 
 function App() {
 	return (
@@ -9,146 +10,7 @@ function App() {
 			<div className="bg">
 				<section className="content">
 					<article>
-						<h1 className="title">Hello my name is caike and I am here!</h1>
-						<p>
-							Alfonso is an alligator with an yellow-orange color, and has a
-							white stomach and chin. Like any alligator, Alfonso has a nose
-							that sticks out and has a huge chomper. He has 5 brown rings on
-							his face and has baggy blue eyes.
-						</p>
-
-						<p>
-							Alfonso's design is based on Nintendo's famous mascot Mario from
-							the Super Mario Bros. series. Not only does he mimic Mario's
-							famous "it'sa me" quote as his initial phrase, but he also wears
-							the big bro's tee, which features the "M" emblem featured on
-							Mario's cap, although the rest of the shirt is red, however, his
-							shirt was changed in New Horizons to the simple parka. Alfonso's
-							external appearance is a subtle reference to Yoshi, Mario's
-							faithful dinosaur companion. He is a burnt-amber orange with
-							darker circles on his face. Both his cheeks and belly are white
-							and his eyes are similarly formed, although his eyelids are a
-							light cyan blue.
-						</p>
-					</article>
-					<article>
-						<h1 className="title">Hello my name is caike and I am here!</h1>
-						<p>
-							Alfonso is an alligator with an yellow-orange color, and has a
-							white stomach and chin. Like any alligator, Alfonso has a nose
-							that sticks out and has a huge chomper. He has 5 brown rings on
-							his face and has baggy blue eyes.
-						</p>
-
-						<p>
-							Alfonso's design is based on Nintendo's famous mascot Mario from
-							the Super Mario Bros. series. Not only does he mimic Mario's
-							famous "it'sa me" quote as his initial phrase, but he also wears
-							the big bro's tee, which features the "M" emblem featured on
-							Mario's cap, although the rest of the shirt is red, however, his
-							shirt was changed in New Horizons to the simple parka. Alfonso's
-							external appearance is a subtle reference to Yoshi, Mario's
-							faithful dinosaur companion. He is a burnt-amber orange with
-							darker circles on his face. Both his cheeks and belly are white
-							and his eyes are similarly formed, although his eyelids are a
-							light cyan blue.
-						</p>
-					</article>
-
-					<article>
-						<h1 className="title">Hello my name is caike and I am here!</h1>
-						<p>
-							Alfonso is an alligator with an yellow-orange color, and has a
-							white stomach and chin. Like any alligator, Alfonso has a nose
-							that sticks out and has a huge chomper. He has 5 brown rings on
-							his face and has baggy blue eyes.
-						</p>
-
-						<p>
-							Alfonso's design is based on Nintendo's famous mascot Mario from
-							the Super Mario Bros. series. Not only does he mimic Mario's
-							famous "it'sa me" quote as his initial phrase, but he also wears
-							the big bro's tee, which features the "M" emblem featured on
-							Mario's cap, although the rest of the shirt is red, however, his
-							shirt was changed in New Horizons to the simple parka. Alfonso's
-							external appearance is a subtle reference to Yoshi, Mario's
-							faithful dinosaur companion. He is a burnt-amber orange with
-							darker circles on his face. Both his cheeks and belly are white
-							and his eyes are similarly formed, although his eyelids are a
-							light cyan blue.
-						</p>
-					</article>
-
-					<article>
-						<h1 className="title">Hello my name is caike and I am here!</h1>
-						<p>
-							Alfonso is an alligator with an yellow-orange color, and has a
-							white stomach and chin. Like any alligator, Alfonso has a nose
-							that sticks out and has a huge chomper. He has 5 brown rings on
-							his face and has baggy blue eyes.
-						</p>
-
-						<p>
-							Alfonso's design is based on Nintendo's famous mascot Mario from
-							the Super Mario Bros. series. Not only does he mimic Mario's
-							famous "it'sa me" quote as his initial phrase, but he also wears
-							the big bro's tee, which features the "M" emblem featured on
-							Mario's cap, although the rest of the shirt is red, however, his
-							shirt was changed in New Horizons to the simple parka. Alfonso's
-							external appearance is a subtle reference to Yoshi, Mario's
-							faithful dinosaur companion. He is a burnt-amber orange with
-							darker circles on his face. Both his cheeks and belly are white
-							and his eyes are similarly formed, although his eyelids are a
-							light cyan blue.
-						</p>
-					</article>
-
-					<article>
-						<h1 className="title">Hello my name is caike and I am here!</h1>
-						<p>
-							Alfonso is an alligator with an yellow-orange color, and has a
-							white stomach and chin. Like any alligator, Alfonso has a nose
-							that sticks out and has a huge chomper. He has 5 brown rings on
-							his face and has baggy blue eyes.
-						</p>
-
-						<p>
-							Alfonso's design is based on Nintendo's famous mascot Mario from
-							the Super Mario Bros. series. Not only does he mimic Mario's
-							famous "it'sa me" quote as his initial phrase, but he also wears
-							the big bro's tee, which features the "M" emblem featured on
-							Mario's cap, although the rest of the shirt is red, however, his
-							shirt was changed in New Horizons to the simple parka. Alfonso's
-							external appearance is a subtle reference to Yoshi, Mario's
-							faithful dinosaur companion. He is a burnt-amber orange with
-							darker circles on his face. Both his cheeks and belly are white
-							and his eyes are similarly formed, although his eyelids are a
-							light cyan blue.
-						</p>
-					</article>
-
-					<article>
-						<h1 className="title">Hello my name is caike and I am here!</h1>
-						<p>
-							Alfonso is an alligator with an yellow-orange color, and has a
-							white stomach and chin. Like any alligator, Alfonso has a nose
-							that sticks out and has a huge chomper. He has 5 brown rings on
-							his face and has baggy blue eyes.
-						</p>
-
-						<p>
-							Alfonso's design is based on Nintendo's famous mascot Mario from
-							the Super Mario Bros. series. Not only does he mimic Mario's
-							famous "it'sa me" quote as his initial phrase, but he also wears
-							the big bro's tee, which features the "M" emblem featured on
-							Mario's cap, although the rest of the shirt is red, however, his
-							shirt was changed in New Horizons to the simple parka. Alfonso's
-							external appearance is a subtle reference to Yoshi, Mario's
-							faithful dinosaur companion. He is a burnt-amber orange with
-							darker circles on his face. Both his cheeks and belly are white
-							and his eyes are similarly formed, although his eyelids are a
-							light cyan blue.
-						</p>
+						<Posts />
 					</article>
 				</section>
 				<footer className="footer">
