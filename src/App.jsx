675d32c7422c9089f853606a2bb59a16af7bce4e
Caike Laurentino <tozeos@outlook.com>
2023-03-23 14:05:46 -0300

    uen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import logo from "./assets/logo.png";
 import leaf from "./assets/Leaf.png";
 import "./App.css";
-import Posts from "./Posts";
+import { postagens } from "./posts";
 
 function App() {
 	return (
@@ -9,9 +9,33 @@ function App() {
 			<img className="logo bounce-7" src={logo} alt="Website logo"></img>
 			<div className="bg">
 				<section className="content">
-					<article>
-						<Posts />
-					</article>
+					{postagens.map((post, i) => (
+						<article key={i}>
+							<h1 className="title">
+								Learning Log {`#` + i + 1} - {post.date}
+							</h1>
+
+							<h3>What did I learn today?</h3>
+
+							<p>{post.p1}</p>
+
+							<h3>What did I like the most?</h3>
+
+							<p>{post.p2}</p>
+
+							<h3>What worked well? What did not?</h3>
+
+							<p>{post.p3}</p>
+
+							<h3>How can I apply this knowledge in my English Classes?</h3>
+
+							<p>{post.p4}</p>
+
+							<h3>What is my grade today about participation?</h3>
+
+							<p>{post.p5}</p>
+						</article>
+					))}
 				</section>
 				<footer className="footer">
 					<a
---
+++ src/Posts.js
@@ -0,0 +1,12 @@
+export const postagens = [
+	{
+		id: 0,
+		date: "March 22nd",
+		p1: "Today I learned soo many things, some stuff I already knew, but many of them I never had heard about. There's so many categories that I've known for so long and others that I haven't. Summarizing everything, I've learned how words are formed, lot's of them I knew, but It was funny knowing more about it.",
+		p2: "I really liked learning all of the ways a word can be formed, and how it changes (or not) the meaning. I kinda like the affixes, they are so usefull and, OMG I just thought about something: if you know the meaning of one of these, you instantly learn a bunch of new words, for example: the words ending -ful, is used for something in quantity, like useful, beautiful, painful. I really like that.",
+		p3: "I took some notes, so I think I got the main points of what the teacher showed us, but I have to study a little bit more by my self to fix all the concepts and examples in my mind. There was so many new concepts that I’m afraid of mix some of them and go crazy on the tests.",
+		p4: "I just remembered the classes in my second Teaching Practicum at UNEB, where me and Vitor taught our students a little about how word formation can help them read better not only English texts but Portuguese and so on. Knowing that helps you guess the meaning of a word you don’t know (in theory). So I believe it really help in reading, and I will always use this in my classes.",
+		p5: "🧠🧠🧠🧠🧠 5 brains out of 5.",
+		img: "",
+	},
+];
